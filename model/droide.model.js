fb2aa01bbb1af1313043ffdaf1b57e09c77f1f33
Yai <yaiza.delrio@clupik.com>
2022-04-02 14:32:28 +0200

    refactor

--- model/droide.model.js
+++ model/droide.model.js
@@ -2,10 +2,21 @@ class Dron {
 	constructor(protocols, scan) {
 		this.protocols = protocols;
 		this.scan = scan;
-		this.shootCoord = null;
-		this.isDoubleProtocol = false;
-		this.firstProtocol = null;
-		this.secondProtocol = null;
+		this.shootCoord = null; //Result
+		this.isDoubleProtocol = protocols.length > 1; //Two or more protocols given
+		this.firstProtocol = null; // Always be closest-enemies or furthest-enemies protocols. These are the ones that get distances
+		this.secondProtocol = null; //Rest of protocols. These are the ones that filter by enemy or ally
+	}
+
+	init() {
+		if (this.isDoubleProtocol) {
+			this.firstProtocol = this.protocols.find((elm) => elm === 'closest-enemies' || elm === 'furthest-enemies');
+			this.secondProtocol = this.protocols.filter((elm) => elm !== 'closest-enemies' && elm !== 'furthest-enemies');
+			this.getProtocol(this.firstProtocol, this.secondProtocol);
+		} else {
+			this.firstProtocol = this.protocols[0];
+			this.getProtocol(this.firstProtocol);
+		}
 	}
 
 	getProtocol(first, second) {
@@ -20,113 +31,59 @@ class Dron {
 		prot[first]();
 	}
 
-	init() {
-		if (this.protocols.length > 1) {
-			this.isDoubleProtocol = true;
-			this.firstProtocol = this.protocols.find((elm) => elm === 'closest-enemies' || elm === 'furthest-enemies');
-			this.secondProtocol = this.protocols.filter((elm) => elm !== 'closest-enemies' && elm !== 'furthest-enemies');
-			this.getProtocol(this.firstProtocol, this.secondProtocol);
-		} else if (this.protocols.length === 1) {
-			this.firstProtocol = this.protocols[0];
-			this.getProtocol(this.firstProtocol);
-		}
-	}
-
 	closestEnemies(type) {
-		if (this.isDoubleProtocol) {
-			const filter = this.filter(type);
-			const closest = filter.reduce((prev, current) => (Math.hypot(prev.coordinates.x, prev.coordinates.y) > Math.hypot(current.coordinates.x, current.coordinates.y) ? current : prev));
-			this.shootCoord = { x: closest.coordinates.x, y: closest.coordinates.y };
-		} else {
-			const scan = [...this.scan];
-			const closest = scan.reduce((prev, current) => (Math.hypot(prev.coordinates.x, prev.coordinates.y) > Math.hypot(current.coordinates.x, current.coordinates.y) ? current : prev));
-			this.shootCoord = { x: closest.coordinates.x, y: closest.coordinates.y };
-		}
+		const scan = this.filterProtocol(type);
+		const closest = scan.reduce((prev, current) => (Math.hypot(prev.coordinates.x, prev.coordinates.y) > Math.hypot(current.coordinates.x, current.coordinates.y) ? current : prev));
+		this.shootCoord = { x: closest.coordinates.x, y: closest.coordinates.y };
 	}
 
 	furthestEnemies(type) {
-		if (this.isDoubleProtocol) {
-			let filter = this.filter(type);
-			filter = filter.filter((elm) => Math.hypot(elm.coordinates.x, elm.coordinates.y) < 100);
-			const furthest = filter.reduce((prev, current) => (Math.hypot(prev.coordinates.x, prev.coordinates.y) > Math.hypot(current.coordinates.x, current.coordinates.y) ? prev : current));
-			this.shootCoord = { x: furthest.coordinates.x, y: furthest.coordinates.y };
-		} else {
-			let scan = [...this.scan];
-			scan = scan.filter((elm) => Math.hypot(elm.coordinates.x, elm.coordinates.y) < 100);
-			const furthest = scan.reduce((prev, current) => (Math.hypot(prev.coordinates.x, prev.coordinates.y) > Math.hypot(current.coordinates.x, current.coordinates.y) ? prev : current));
-			this.shootCoord = { x: furthest.coordinates.x, y: furthest.coordinates.y };
-		}
+		let scan = this.filterProtocol(type);
+		scan = scan.filter((elm) => Math.hypot(elm.coordinates.x, elm.coordinates.y) < 100); //Always distances under 100m
+		const furthest = scan.reduce((prev, current) => (Math.hypot(prev.coordinates.x, prev.coordinates.y) > Math.hypot(current.coordinates.x, current.coordinates.y) ? prev : current));
+		this.shootCoord = { x: furthest.coordinates.x, y: furthest.coordinates.y };
 	}
 
 	assistAllies() {
-		const scan = this.scan.filter((elm) => elm.hasOwnProperty('allies'));
-		this.shootCoord = { x: scan[0].coordinates.x, y: scan[0].coordinates.y };
+		const scan = this.scan.find((elm) => elm.hasOwnProperty('allies'));
+		this.shootCoord = { x: scan.coordinates.x, y: scan.coordinates.y };
 	}
 
 	avoidCrossFire() {
-		const scan = this.scan.filter((elm) => !elm.hasOwnProperty('allies'));
-		this.shootCoord = { x: scan[0].coordinates.x, y: scan[0].coordinates.y };
+		const scan = this.scan.find((elm) => !elm.hasOwnProperty('allies'));
+		this.shootCoord = { x: scan.coordinates.x, y: scan.coordinates.y };
 	}
 
 	prioritizeMech() {
-		const scan = this.scan.filter((elm) => elm.enemies.type === 'mech');
-		if (scan.length > 0) {
-			this.shootCoord = scan[0].coordinates;
-		}
+		const scan = this.scan.find((elm) => elm.enemies.type === 'mech');
+		this.shootCoord = { x: scan.coordinates.x, y: scan.coordinates.y };
 	}
 
 	avoidMech() {
-		const scan = this.scan.filter((elm) => elm.enemies.type !== 'mech');
-		this.shootCoord = scan[0].coordinates;
+		const scan = this.scan.find((elm) => elm.enemies.type !== 'mech');
+		this.shootCoord = { x: scan.coordinates.x, y: scan.coordinates.y };
 	}
 
-	filter(type) {
-		let protocol, result;
-		if (type.length > 1) {
-			protocol = type.join('//');
+	filterProtocol(secProtocol) {
+		//In case of more than one protocol, this method filters the scan array by enemy or ally.
+		//In case of just one, it returns a copy of the scan array
+		let auxScan = [...this.scan];
+		const filterFunctions = {
+			'assist-allies': (arr) => arr.filter((elm) => elm.hasOwnProperty('allies')),
+			'avoid-crossfire': (arr) => arr.filter((elm) => !elm.hasOwnProperty('allies')),
+			'prioritize-mech': (arr) => arr.filter((elm) => elm.enemies.type === 'mech'),
+			'avoid-mech': (arr) => arr.filter((elm) => elm.enemies.type !== 'mech'),
+		};
+		if (!this.isDoubleProtocol) {
+			return auxScan;
 		} else {
-			protocol = type[0];
-		}
-
-		switch (protocol) {
-			case 'prioritize-mech//avoid-crossfire':
-			case 'avoid-crossfire//prioritize-mech':
-				result = this.scan.filter((elm) => elm.enemies.type === 'mech');
-				result = result.filter((elm) => !elm.hasOwnProperty('allies'));
-				return result;
-				break;
-			case 'prioritize-mech//assist-allies':
-			case 'assist-allies//prioritize-mech':
-				result = this.scan.filter((elm) => elm.enemies.type === 'mech');
-				result = result.filter((elm) => elm.hasOwnProperty('allies'));
-				return result;
-				break;
-			case 'avoid-mech//assist-allies':
-			case 'assist-allies//avoid-mech':
-				result = this.scan.filter((elm) => elm.enemies.type !== 'mech');
-				result = result.filter((elm) => elm.hasOwnProperty('allies'));
-				return result;
-				break;
-			case 'avoid-mech//avoid-crossfire':
-			case 'avoid-crossfire//avoid-crossfire':
-				result = this.scan.filter((elm) => elm.enemies.type !== 'mech');
-				result = result.filter((elm) => !elm.hasOwnProperty('allies'));
-				return result;
-				break;
-			case 'assist-allies':
-				return this.scan.filter((elm) => elm.hasOwnProperty('allies'));
-				break;
-			case 'avoid-crossfire':
-				return this.scan.filter((elm) => !elm.hasOwnProperty('allies'));
-				break;
-			case 'prioritize-mech':
-				return this.scan.filter((elm) => elm.enemies.type === 'mech');
-				break;
-			case 'avoid-mech':
-				return this.scan.filter((elm) => elm.enemies.type !== 'mech');
-				break;
-			default:
-				break;
+			//According to the given protocols, there can only be a maximum of 3 combinations, so there is only the possibility to filter by enemy type and/or allies.
+			if (secProtocol.length === 2) {
+				let firstPass = filterFunctions[secProtocol[0]](auxScan);
+				return filterFunctions[secProtocol[1]](firstPass);
+			} else if (secProtocol.length === 1) {
+				return filterFunctions[secProtocol[0]](auxScan);
+			}
 		}
 	}
 }
